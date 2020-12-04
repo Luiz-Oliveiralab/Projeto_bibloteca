5b5c47e6cc7e40540814b770b750b48033679bf5
SuperNicolau123 <nickgenio2004@gmail.com>
2020-12-03 22:27:41 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import DadosNome from "./components/DadosNome"
+import DadosIdade from "./components/DadosIdade"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DadosNome tipo="nome"></DadosNome>
+       <DadosIdade tipo="idade"></DadosIdade>
     </div>
   );
 }
