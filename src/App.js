09e64c465e5afd7be4a292ea3357c12b2887534d
SuperNicolau123 <nickgenio2004@gmail.com>
2020-12-07 11:09:16 -0300

    A

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import DadosNome from "./components/DadosNome"
-import DadosIdade from "./components/DadosIdade"
 import DadosAtividade from "./components/DadosAtividade"
-import DadosTurma from "./components/DadosTurma"
-import DadosCurso from "./components/DadosCurso"
 
 function App() {
   return (
     <div className="App">
-       <DadosNome tipo="nome"></DadosNome>
-       <DadosIdade tipo="idade"></DadosIdade> 
-       <DadosAtividade tipo="atividade"></DadosAtividade>
-       <DadosTurma tipo="turma"></DadosTurma>
-       <DadosCurso tipo="curso"></DadosCurso>
+       <div className="centro">
+          <DadosAtividade tipo="atividade"></DadosAtividade>
+       </div>
     </div>
   );
 }
--- src/components/DadosAtividade.js
+++ src/components/DadosAtividade.js
@@ -1,22 +1,46 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import './DadosAtividade.css'
+import logo from './logo-senai.png'
 
-export default class DadosAtividade extends Component{
+export default class DadosAtividade extends Component {
 
-    constructor(props){
-        super (props);
+    constructor(props) {
+        super(props);
 
         this.state = {
             atividade: "",
+            nome: "1",
+            idade: "2",
+            data: "3",
+            hora: "4",
+            curso: "5",
+            turma: "6",
+            numero: "7",
         }
     }
 
-
-
-    render () {
+    render() {
         return (
             <div className="dadosatividade">
-                <input type="text" onChange={(event)=>{this.setState({atividade:event.target.value})}}></input>
-                <h6>Atividade</h6>
+                <div className='linha'>
+                    <div className='atividade'>
+                        <input type="text" onChange={(event) => { this.setState({ atividade: event.target.value }) }}></input>
+                        <h6>Atividade</h6>
+                    </div>
+                    <img src={logo} width='300px' height='50px'></img>
+                </div>
+                <div className='linha'>
+                    <h2>{this.state.nome}</h2>
+                    <h2>{this.state.idade}</h2>
+                </div>
+                <div className='linha'>
+                    <h2>{this.state.curso}</h2>
+                    <h2>{this.state.turma}</h2>
+                </div>
+                <div className='linha'>
+                    <h2>{this.state.numero}</h2>
+                    <h2>{this.state.data} e {this.state.hora}</h2>
+                </div>
             </div>
         )
     }
